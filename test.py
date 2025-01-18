fca9d29e351016c78dcb4700feb633238b7194d2
Arijit9721 <das03285@gmail.com>
2025-01-18 23:14:54 +0530

    added new feature

--- test.py
+++ test.py
@@ -10,7 +10,6 @@ print(full_name)
 
 print("this code was done to checkout a new branch")
 print("this code was created to checkout the merge feature")
-
 print("this code was created to checkout the rebase feature")
 print("this code was created to checkout the rebase feature")
 
